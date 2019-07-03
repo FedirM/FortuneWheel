66614d29231643e8254c7ac4447edc4b57ef5297
unknown <tedmoster@gmail.com>
2019-07-03 12:31:12 +0300

    Add wheel animation on button click

--- assets/script/main.ts
+++ assets/script/main.ts
@@ -7,11 +7,17 @@ export default class NewClass extends cc.Component {
     @property(cc.Sprite)
     background: cc.Sprite = null;
 
+    @property(cc.Sprite)
+    wheel: cc.Sprite = null;
+
 
     // LIFE-CYCLE CALLBACKS:
 
     onLoad () {
-        
+        let buttonLocation = "Canvas/btn";
+        let nodeButton = cc.find(buttonLocation).getComponent(cc.Button);
+
+        nodeButton.node.on(cc.Node.EventType.TOUCH_START.toString(), this.onSpinClicked, this);
     }
 
     start () {
@@ -19,8 +25,19 @@ export default class NewClass extends cc.Component {
     }
 
     update (dt) {
-        console.log(`width: ${this.node.width}\theight: ${this.node.height}`);
+        
+        //console.log(`width: ${this.node.width}\theight: ${this.node.height}`);
         this.background.node.width = this.node.width;
         this.background.node.height = this.node.height;
+    }
+
+    onSpinClicked(event){
+        let rnum = Math.round(Math.random() * (17 - 1) + 1);
+        cc.log("Random num: ", rnum);
+
+        let anim = this.wheel.getComponent(cc.Animation);
+        let animName = 'wheel-'+rnum;
+        cc.log("Trying play: ", animName);
+        anim.play(animName);
     }
 }
